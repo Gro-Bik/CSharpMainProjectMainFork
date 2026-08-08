65f59f99bc1d92a97f34511c6831234f8d778b90
Niklle <amx50100tank@gmail.com>
2026-08-08 10:58:56 +0300

    Homework 1 done Name in telegram: Niklle

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -33,7 +33,7 @@ namespace UnitBrains.Player
             }
 
             IncreaseTemperature();
-            //Имя в телеграмме Niklle(@Nick_Fumo_Konager)
+            //Мое имя в телеграмме Niklle(@Nick_Fumo_Konager)
         }
 
         public override Vector2Int GetNextStep()
